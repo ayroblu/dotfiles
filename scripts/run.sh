3fbb0507e79b360d82a504808138d5ddc14f7d5a
Ben Lu <ayroblu@gmail.com>
2021-04-05 11:48:16 +0100

    Add stack overflow link for explanation of regex

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for file in .*; do
   list=". .. .git .DS_Store"
+  # https://stackoverflow.com/questions/8063228/check-if-a-variable-exists-in-a-list-in-bash
   if ! [[ $list =~ (^|[[:space:]])$file($|[[:space:]]) ]]; then
     if [ -e ~/"$file" ]; then
       echo ~/"$file" exists
