bf672e7d9544e55107ed430076c9893c6eedbcb0
Ben Lu <ayroblu@gmail.com>
2021-10-31 19:40:38 +0000

    Support environment commands like cd in alfred

--- alfred-terminal.sh
+++ alfred-terminal.sh
@@ -1,5 +1,4 @@
 set -x
-zsh -c "${1:-echo "No command passed"}"
+zsh -c "${1:-echo "No command passed"}; zsh"
 # https://stackoverflow.com/questions/13195655/bash-set-x-without-it-being-printed
 { set +x; } 2>/dev/null
-zsh
