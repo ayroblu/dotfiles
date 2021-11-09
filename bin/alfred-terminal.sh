65f67fc456e3915cd3240f318714a626ea6a81f1
Ben Lu <ayroblu@gmail.com>
2021-11-09 23:39:46 +0000

    Don't auto prettier markdown files, alfred terminal passes SHELL variable

--- bin/alfred-terminal.sh
+++ bin/alfred-terminal.sh
@@ -1,4 +1,4 @@
 set -x
-zsh -c "${1:-echo "No command passed"}; zsh"
+SHELL=/bin/zsh zsh -c "${1:-echo "No command passed"}; zsh"
 # https://stackoverflow.com/questions/13195655/bash-set-x-without-it-being-printed
 { set +x; } 2>/dev/null
