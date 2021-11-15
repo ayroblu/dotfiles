3f342515e6c7773e69edc76b20138456ee799652
Ben Lu <ayroblu@gmail.com>
2021-11-15 22:49:48 +0000

    Update scripts to include .vim/coc-settings.json

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ~/bin
-for file in .* bin/*; do
-  list=". .. .git .DS_Store"
+for file in .* bin/* .vim/*; do
+  list=". .. .git .DS_Store .vim"
   # https://stackoverflow.com/questions/8063228/check-if-a-variable-exists-in-a-list-in-bash
   if ! [[ $list =~ (^|[[:space:]])$file($|[[:space:]]) ]]; then
     if [ -e ~/"$file" ]; then
