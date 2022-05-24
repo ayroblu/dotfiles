ede8e12c5e9026ab89598330cfcadefff9d5eb4a
Ben Lu <benlu@twitter.com>
2022-05-24 18:43:03 +0100

    Add echo path

--- .sharedrc.sh
+++ .sharedrc.sh
@@ -33,6 +33,8 @@ alias vigit="vi ~/.gitconfig*"
 alias reindexspotlight='sudo mdutil -E /'
 # https://vi.stackexchange.com/questions/4682/how-can-i-suppress-the-reading-from-stdin-message-from-within-vim
 alias vpage="vim -c 'set ft=man ts=8 nomod nolist nonu noma' --not-a-term -"
+# https://unix.stackexchange.com/questions/80151/show-path-in-a-human-readable-way
+alias echopath='echo -e ${PATH//:/\\n}'
 # for sharing clipboard: deprecated, can just use bin/pbcopy with escape codes - keeping around as it might be useful?
 # alias sshpb='ssh -R 2324:localhost:2324 -R 2325:localhost:2325'
 # exists pbcopy || alias pbcopy='nc -q0 localhost 2324'
