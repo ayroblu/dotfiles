efe872d203a1b0b17427ab16f87dbffbad1429c7
Ben Lu <benlu@twitter.com>
2023-03-19 22:03:57 +0000

    symlink whole nvim directory

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 mkdir -p ~/bin
-mkdir -p ~/.config/nvim
 mkdir -p ~/.config/bat
-for file in .* bin/* .vim/* .config/nvim/* .config/bat/*; do
+for file in .* bin/* .vim/* .config/nvim .config/bat/*; do
   ignorelist=". .. .git .DS_Store .vim .config"
   # https://stackoverflow.com/questions/8063228/check-if-a-variable-exists-in-a-list-in-bash
   if ! [[ $ignorelist =~ (^|[[:space:]])$file($|[[:space:]]) ]]; then
