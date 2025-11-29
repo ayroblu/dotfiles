d522e565b2e06b7fee27874fe49ca2971fccdc03
Ben Lu <ayroblu@gmail.com>
2025-11-29 16:30:49 +0000

    add vsnip

--- .config/nvim/lua/lsp-setup.lua
+++ .config/nvim/lua/lsp-setup.lua
@@ -10,6 +10,16 @@ end
 
 local function setupCmp()
   local lspkind = require('lspkind')
+
+  local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
+  local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+  end
+
   local cmp = require 'cmp'
   cmp.setup({
     formatting = {
@@ -28,6 +38,7 @@ local function setupCmp()
       end,
     },
     sources = cmp.config.sources({
+      { name = 'vsnip' },
       { name = 'nvim_lsp' },
       { name = 'ultisnips' },
     }),
@@ -39,6 +50,26 @@ local function setupCmp()
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
       ['<C-space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
+
+      -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#vim-vsnip
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif vim.fn["vsnip#available"](1) == 1 then
+          feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function()
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+          feedkey("<Plug>(vsnip-jump-prev)", "")
+        end
+      end, { "i", "s" }),
     },
   })
 end
