a70bb45eb3ed1d2943d7e803ed078aa4ad722c40
Ben Lu <ayroblu@gmail.com>
2025-11-29 13:48:20 +0000

    support tabs in git-delta, and application support

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 mkdir -p ~/bin
 mkdir -p ~/.vim
-for file in .* bin/* .vim/* .config/*; do
-  ignorelist=". .. .git .DS_Store .vim .config"
+for file in .* bin/* .vim/* .config/* Library/Application\ Support/*; do
+  ignorelist=". .. .git .DS_Store .vim .config Library/Application\ Support"
   # https://stackoverflow.com/questions/8063228/check-if-a-variable-exists-in-a-list-in-bash
   if ! [[ $ignorelist =~ (^|[[:space:]])$file($|[[:space:]]) ]]; then
     if [ -e ~/"$file" ]; then
